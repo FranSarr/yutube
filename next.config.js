56a28d399357c693b13b0a375e8933bbe078d74a
fsarrias <fsarrias@kolab.es>
2022-06-22 10:56:54 +0200

    I show the thumnails of videos

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost',
+      'bucketkikus.s3.eu-west-3.amazonaws.com',
+      'bucketkikus.s3.amazonaws.com',
+    ],
+  },
 }
+
+
+module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
 import Head from 'next/head'
+import { getVideos } from 'lib/data.js'
+import prisma from 'lib/prisma'
+import Videos from 'components/Videos'
 
-export default function Home() {
+export default function Home({videos}) {
   return (
     <div>
-      <Head>
-        <title></title>
-        <meta name='description' content='' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <Head>
+      <title>YouTube Clone</title>
+      <meta name='description' content='A great YouTube Clone' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <header className='h-14 flex pt-5 px-5 pb-2'>
+      <div className='text-xl'>
+        <p>YouTube clone</p>
+      </div>
 
-      <h1>Welcome!</h1>
-    </div>
+      <div className='grow'></div>
+    </header>
+
+    {videos.length === 0 && (
+      <p className='flex justify-center mt-20'>No videos found!</p>
+    )}
+    <Videos videos={videos} />
+  </div>
   )
 }
+export async function getServerSideProps() {
+    let videos = await getVideos({}, prisma)
+      videos = JSON.parse(JSON.stringify(videos))
+  
+    return {
+      props: {
+        videos,
+      },
+    }
+  }
